1efc4d2cf502c5165555fe88e7bad118739dd997
Riley Jones <rileyjones109@gmail.com>
2017-10-12 17:13:00 -0700

    Make Environment use any type of Drawable

--- src/Startup.java
+++ src/Startup.java
@@ -5,7 +5,6 @@ public class Startup {
         int width = 720;
         int height = 480;
         GraphicsPanel window = new GraphicsPanel(width, height);
-        Environment environ = new Environment(width,height,20,20,10,window);
         Point primary = new Point(0,5,20);
         Point extra1 = new Point(-12,-12,20);
         Point extra2 = new Point(12,-12,20);
@@ -13,7 +12,7 @@ public class Startup {
         Point upVector=extra1.add(vector3.multiply(.5)).makeRelative(primary).multiply(-.5);
         File imageLocation =new File("insert file location here");
         Polygon main = new Polygon(primary,extra1,extra2,25,extra1.add(vector3.multiply(.1)),imageLocation,1);
-        environ.addPolygon(main);
+        Environment environ = new Environment(width,height,20,20,10,window,main);
         new Thread(window).start();
         new Thread(new Timer(200,environ,window)).start();   //Pass objects in the order to be updated
 
